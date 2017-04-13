88a05877d85e1804ea5f25453cfdbaea2fca06f3
kinathru <kinath.ru@gmail.com>
2017-04-13 16:36:17 +0530

    initial commit

---
+++ src/app/main/main.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainComponentComponent } from './main-component/main-component.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MainComponentComponent]
+})
+export class MainModule { }
